da3915416e5d7d3491c338cd8b2f715c13f64d4a
Sean Baier <sean.t.baier@gmail.com>
2020-06-14 23:01:07 -0500

    working users components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import SignIn from './components/auth/SignIn'
 import ForgotPassword from './components/auth/ForgotPassword'
 import ForgotPasswordSubmit from './components/auth/ForgotPasswordSubmit'
 import Amplify from 'aws-amplify'
+import { Provider } from 'react-redux'
+import configureStore from './store'
 
 // Components
 import View from './views'
@@ -32,11 +34,15 @@ AmplifyCustomUi.configure(aws_amplify_react)
 AmplifyCustomUi.setSignIn(SignIn)
 AmplifyCustomUi.setForgotPassword(ForgotPassword)
 
+const store = configureStore()
+
 const App = () => (
-  <div>
-    <AmplifySignOut />
-    <View />
-  </div>
+  <Provider store={store}>
+    <div>
+      <AmplifySignOut />
+      <View />
+    </div>
+  </Provider>
 )
 
 export default withAuthenticator(App)
